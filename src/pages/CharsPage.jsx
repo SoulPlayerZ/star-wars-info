5580e1629f05865b2b693753a4bc46c04265b7bf
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-13 23:58:47 -0300

    feat: Implement getChars. Fetching chars (people) of API

--- src/pages/CharsPage.jsx
+++ src/pages/CharsPage.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import getChars from "../services/fetchs";
 
 function CharsPage () {
+
+    const a = async () => {
+      console.log(await getChars());
+    }
+
+
+  useEffect(() => {
+    a();
+  },[])
+
   return(
     <section>
       <Header />
+      <Footer />
     </section>
   )
 }
