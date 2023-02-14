d8c0dfdb5b7ee396795f2d39b9af5921acde1164
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-13 23:31:33 -0300

    feat: Implement  route  "/"

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 function App() {
  
   return (
-    <div>
-     Hello World
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>HEADER</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
