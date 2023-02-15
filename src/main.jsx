b946df6a87e15f7789fcd3b4542e8cf5060894e0
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-15 18:18:32 -0300

    feat: Implement render Logic

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import CharsProvider from './context/CharsProvider';
 import HamburguerProvider from './context/HamburguerProvider';
+import LoadingProvider from './context/LoadingProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HamburguerProvider>
       <CharsProvider>
-        <App />
+        <LoadingProvider>
+          <App />
+        </LoadingProvider>
       </CharsProvider>
     </HamburguerProvider>
   </React.StrictMode>,
--- src/pages/CharsPage.jsx
+++ src/pages/CharsPage.jsx
@@ -1,15 +1,22 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import getChars from "../services/fetchs";
+import CharsContext from "../context/CharsContext";
+import LoadingContext from "../context/LoadingContext";
 import "../css/mainPage.css";
 
 function CharsPage () {
+  const { chars, setChars } = useContext(CharsContext);
+  const { loading, setLoading } = useContext(LoadingContext);
 
-    const getCharsFromAPI = async () => {
-      console.log(await getChars());
-    }
 
+  const getCharsFromAPI = async () => {
+    setLoading(!loading);
+    const charsReturned = await getChars()
+    setChars(charsReturned);
+    setLoading(!loading);
+  }
 
   useEffect(() => {
     getCharsFromAPI();
@@ -18,6 +25,7 @@ function CharsPage () {
   return(
     <section className="mainPage">
       <Header />
+      {chars.results === undefined ? <h3>TEM</h3> : console.log(chars)}
       <Footer />
     </section>
   )
