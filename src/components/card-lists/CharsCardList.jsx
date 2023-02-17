e5ef06d355a12ca2682b61bfc77b68c01ee001c8
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-16 22:54:10 -0300

    feat: Create CharCard to map on CharsCardList

--- src/components/card-lists/CharsCardList.jsx
+++ src/components/card-lists/CharsCardList.jsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import CharsContext from "../../context/CharsContext";
+import CharCard from "../cards/CharCard";
 
 function CharsCardList () {
   const { chars } = useContext(CharsContext);
 
   return(
-    chars.results.map((char) => <CharCard key={ char.name } />)
+    chars.results.map((char) => <CharCard key={ char.name } char={ char } />)
    
   )
 }
---
+++ src/components/cards/CharCard.jsx
@@ -0,0 +1,10 @@
+function CharCard ({ char }) {
+
+//   console.log(char);
+
+  return(
+    <h2 className="loading"> {char.name} </h2>
+  )
+}
+
+export default CharCard;
